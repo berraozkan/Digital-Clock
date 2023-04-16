a5e7cda6e9c8576043cdf12eadeba6db6d2959ae
berraozkan <124502266+berraozkan@users.noreply.github.com>
2023-04-17 00:38:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ function updateClock() {
         ampm = "PM";
     }
 
-    h = h<10 ? "0" + h : h
-    m = m<10 ? "0" + m : m 
-    s = s<10 ? "0" + s : s  
+    h = h < 10 ? "0" + h : h
+    m = m < 10 ? "0" + m : m
+    s = s < 10 ? "0" + s : s
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampmEl,(innerText = ampm);
-    setTimeout(()=>{
+    ampmEl, (innerText = ampm);
+    setTimeout(() => {
         updateClock()
-    },1000)
+    }, 1000)
 }
 
 updateClock()
